a388e2b6af79385e39d69d63d5609adb87265de8
AvielSrypnyk <skrypnykaviel@gmail.com>
2023-09-06 20:37:47 +0200

    Update les12_opdr.py

--- Deel_2/Les_12/uitwerkingen/les12_opdr.py
+++ Deel_2/Les_12/uitwerkingen/les12_opdr.py
@@ -5,9 +5,18 @@ naam = input("Wat is je naam? ")
 leeftijd = int(input("Hoe oud ben je? "))
 
 print(f"{naam} je bent over 5 jaar {leeftijd + 5} oud")
+
 # Opdracht 2:
 # Schrijf een programma dat de gebruiker vraagt om twee getallen in te voeren. Gebruik vervolgens typecasting om de getallen om te zetten in floats en bereken het gemiddelde van de twee getallen.
+getal1 = float(input("Vul een nummer: "))
+getal2 = float(input("Vul een nummer: "))
+
+gemiddelde = ((getal1 + getal2) / 2 )
+print(f"Het is gemidelde " , gemiddelde)
 
 # Opdracht 3:
 # Schrijf een programma dat de gebruiker vraagt om een woord in te voeren. Print vervolgens het volgende:
 # "Het woord is: {woord} en bestaat uit {aantal_tekens} tekens."
+woord = input("Wat is een woord? ")
+aantal_tekens = len(woord)
+print(f"Het woord is: {woord} en bestaat uit {aantal_tekens} tekens.")
