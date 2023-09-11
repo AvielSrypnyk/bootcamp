8be7c94d4fc8620551caa04a8cc187ce40a0977e
AvielSrypnyk <skrypnykaviel@gmail.com>
2023-09-11 11:31:20 +0200

    Create les15_opdr2.py

---
+++ Deel_2/Les_15/uitwerkingen/les15_opdr2.py
@@ -0,0 +1,13 @@
+# Opdracht 2:
+# Schrijf een functie met twee parameters (integers).
+# De functie retourneert de vermenigvuldiging van de twee parameters.
+
+def keerfunctie(getal1 , getal2):
+    keer = getal1 * getal2
+    return keer
+
+
+getal1 = 10
+getal2 = 5
+resultaat = keerfunctie(getal1, getal2)
+print(f"De som van {getal1} en {getal2} is {resultaat}")
